9ea027bb31f7efdedfeb8c697690aaa96963712b
Vadimostapchuk <90713995+Vadimostapchuk@users.noreply.github.com>
2022-02-27 08:40:59 +0300

    новый сайт

--- src/php/mail.php
+++ src/php/mail.php
@@ -5,21 +5,13 @@ $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
 
 $name = $_POST['user_name'];
-$email = $_POST['user_email'];
-$phone = $_POST['user_text'];
+$email = $_POST['user_phone'];
 
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'ostv05@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'Rd14rcntg'; // Ваш пароль от почты с которой будут отправляться письма
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
 $mail->setFrom('ostv05@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('molaxit500@veb65.com');     // Кому будет уходить письмо 
+$mail->addAddress('vadim.ostapchuk.02@bk.ru');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
@@ -29,7 +21,7 @@ $mail->addAddress('molaxit500@veb65.com');     // Кому будет уходи
 $mail->isHTML(true);                                  // Set email format to HTML
 
 $mail->Subject = 'Заявка с сайта';
-$mail->Body    = '' .$email . ' оставил заявку!';
+$mail->Body    = '' .$email . ' оставил заявку!'.'' .$name . ' - имя пользователя' .'' .$phone . ' - текст сообщения';
 $mail->AltBody = '';
 
 if(!$mail->send()) {
