ffa44673b6f2e79b6414a10cafbf642d68be7ebd
Vadimostapchuk <90713995+Vadimostapchuk@users.noreply.github.com>
2022-03-16 22:05:03 +0300

    Upload code

--- src/js/miScript.js
+++ src/js/miScript.js
@@ -11,3 +11,20 @@
   	$('.foto2').magnificPopup({type:'image'});
 });
 
+$(document).ready(function() {
+	$('.form').submit(function(event){
+		event.preventDefault();
+
+		$.ajax({
+			type: "POST",
+			url: "php/mail.php",
+			data: $(this).serialize()
+		}).done(function(){
+			$(this).find("input").val(" ");
+			alert("Успешно отправлено!");
+			$("form").trigger("reset");
+		});
+		return false;
+	});
+
+});	
